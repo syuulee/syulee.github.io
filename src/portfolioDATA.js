fdf338c465105cf55c503f5eff161b548c07c46e
syuulee <yuyu00lee@gmail.com>
2022-10-12 17:49:45 +0900

    2022.10.12.

---
+++ src/portfolioDATA.js
@@ -0,0 +1,11 @@
+const PD = [
+    { id: 1, title: "portfolio_01", color: ['#333', '#f00', '#369'], content: "first homepage", font: "폰트정보", link: "syuulee.github.io/", anchor: "pf01" },
+    { id: 2, title: "portfolio_02", color: ['#f00', '#00f'], content: "second homepage", font: "폰트정보", link: "syuulee.github.io/", anchor: "pf02" },
+    { id: 3, title: "portfolio_03", color: ['#16f', '#9f0'], content: "third homepage", font: "폰트정보", link: "syuulee.github.io/", anchor: "pf03" },
+    { id: 4, title: "portfolio_04", color: ['#999', '#99f'], content: "third homepage", font: "폰트정보", link: "syuulee.github.io/", anchor: "pf04" },
+    { id: 5, title: "portfolio_05", color: ['#999', '#99f'], content: "third homepage", font: "폰트정보", link: "syuulee.github.io/", anchor: "pf05" },
+    { id: 6, title: "portfolio_06", color: ['#999', '#99f'], content: "third homepage", font: "폰트정보", link: "syuulee.github.io/", anchor: "pf06" },
+]
+
+
+export default PD; 
