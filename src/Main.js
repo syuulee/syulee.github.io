9f383f88e7bf1bc6b14d851f570eedd5c5df9577
syuulee <yuyu00lee@gmail.com>
2022-10-17 17:50:16 +0900

    2022.10.17

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 import AnimatedCursor from "react-animated-cursor";
+import useScrollFadeIn from './useScrollFadeIn';
 import { profile, portfolio } from './portfolioDATA';
+import { portfolioSub } from './portfolioSubData';
 import './main.scss';
+import Goo from 'gooey-react';
 
 // portfolio배열에서 title을 빼서 배열을 만들어 쓰겠다.
-const ac = portfolio.map(it => it = it.title);
+const ac = portfolio.map(it => it.anchor);
 
 const Cover = ({ on, setOn }) => {
-    const cover = useRef(null);
+    const cover = useRef(null)
     const wheelStop = e => {
         e.stopPropagation();
     }
@@ -19,13 +22,13 @@ const Cover = ({ on, setOn }) => {
         }
     }, [])
     return (
-        <div className={`Cover ${on ? 'on' : ''}`} ref={cover}>
+        <div className={`cover ${on ? "on" : ""}`} ref={cover}>
             <ul>
                 {
                     portfolio.map((it, idx) => {
                         return (
                             <li key={idx} onClick={() => setOn(!on)}>
-                                <a href={`#${it.title}`}>{it.title}</a>
+                                <a href={`#${it.anchor}`}>{it.anchor}</a>
                             </li>
                         )
                     })
@@ -35,18 +38,18 @@ const Cover = ({ on, setOn }) => {
     )
 }
 
-
 const Main = () => {
     const [num, setNum] = useState(1);
     const [on, setOn] = useState(false);
+    const animatedItem = useScrollFadeIn();
 
     return (
         <div className='FP'>
-            {/* <Cover on={on} setOn={setOn} />
+            <Cover on={on} setOn={setOn} />
             <button className={`btn ${on ? "on" : ""}`} onClick={() => setOn(!on)}>
                 <span>cover open</span>
-            </button> */}
-            {/* <div className='num'>{portfolio[num - 1]?.title}</div> */}
+            </button>
+            {/* <div className='num'>{portfolio[num - 2]?.title}</div> */}
             <header className='header'>
                 <h1>2022 프론트엔드 포트폴리오</h1>
                 <p className='fix'>Frontend portfolio</p>
@@ -79,9 +82,9 @@ const Main = () => {
                         <ReactFullpage.Wrapper>
                             <div className="section section_cover">
                                 <div className="inner">
-                                    <h2>I'm the one<br />
-                                        you're looking for<br />
-                                        미숫가루 먹고싶다.<br />
+                                    <h2>넓은<br />
+                                        하늘로의<br />
+                                        비상을 꿈꾸며<br />
                                     </h2>
                                     <div className="topic">
                                         <p className='top'>감자 감자 왕감자 <br />
@@ -104,7 +107,8 @@ const Main = () => {
                                     return (
                                         <div className="section">
                                             <div className='case'>
-                                                <div className="container">
+                                                {/* {...animatedItem} */}
+                                                <div className="container"  >
                                                     <div className="photo">
                                                         <img src={it.src} alt={it.title} />
                                                     </div>
@@ -113,6 +117,20 @@ const Main = () => {
                                                         <li className='type'>{it.type}</li>
                                                         <li className='info'>
                                                             <p>{it.info}</p>
+                                                        </li>
+                                                        <li className='skill'>
+                                                            {
+                                                                it.skill && <strong>skill</strong>
+                                                            }
+                                                            <ol>
+                                                                {
+                                                                    it.skill?.map((skill, idx) => {
+                                                                        return (
+                                                                            <li>{skill}</li>
+                                                                        )
+                                                                    })
+                                                                }
+                                                            </ol>
                                                         </li>
                                                         <li className='color'>
                                                             {
@@ -130,25 +148,49 @@ const Main = () => {
                                                         </li>
                                                         <li className='link'>
                                                             <a href={it.link} target='_blank'>
-                                                                <button>LINK</button>
+                                                                <button>view more</button>
                                                             </a>
                                                         </li>
                                                     </ul>
                                                 </div>
-
                                             </div>
                                         </div>
                                     )
                                 })
                             }
-                            <div className="section">
-                                <p>Section 2</p>
+                            <div className="section last_section">
+                                <div className="inner">
+                                    <h2>프로젝트</h2>
+                                    <p>Section 2</p>
+                                    <ul className='project_case'>
+                                        {
+                                            portfolioSub.map(it => {
+                                                return (
+                                                    <div className="project">
+                                                        <figure>
+                                                            <img src={it.src} alt={it.title} />
+                                                        </figure>
+                                                        <ul className="desc">
+                                                            <li className='title'>{it.title}</li>
+                                                            <li className='type'>{it.type}</li>
+                                                            <li className='link'>
+                                                                <a href={it.link} target='_blank'>
+                                                                    <button>view more</button>
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </div>
                             </div>
-                        </ReactFullpage.Wrapper>
+                        </ReactFullpage.Wrapper >
                     );
                 }}
             />
-            < svg class="cursor" width="220" height="220" viewBox="0 0 220 220" >
+            {/* < svg class="cursor" width="220" height="220" viewBox="0 0 220 220" >
                 <defs>
                     <filter id="filter-1" x="-50%" y="-50%" width="200%" height="200%"
                         filterUnits="objectBoundingBox">
@@ -159,7 +201,7 @@ const Main = () => {
                 </defs>
                 <circle class="cursor__inner" cx="110" cy="110" r="60" />
             </svg>
-            <script src="js/Animate.js"></script>
+            <script src="js/Animate.js"></script> */}
         </div >
     )
 }
---
+++ src/portfolioSubData.js
@@ -0,0 +1,11 @@
+
+const portfolioSub = [
+    { id: 1, title: "오설록", type: "Responsive Web", font: "Noto Sans Korean", src: process.env.PUBLIC_URL + "/assets/img/ndg.jpg", link: "http://desipossa.github.io/", anchor: "오설록" },
+    { id: 2, title: "오설록", type: "Responsive Web", font: "Noto Sans Korean", src: process.env.PUBLIC_URL + "/assets/img/ndg.jpg", link: "http://desipossa.github.io/", anchor: "오설록" },
+    { id: 3, title: "오설록", type: "Responsive Web", font: "Noto Sans Korean", src: process.env.PUBLIC_URL + "/assets/img/ndg.jpg", link: "http://desipossa.github.io/", anchor: "오설록" },
+    { id: 4, title: "오설록", type: "Responsive Web", font: "Noto Sans Korean", src: process.env.PUBLIC_URL + "/assets/img/ndg.jpg", link: "http://desipossa.github.io/", anchor: "오설록" },
+    { id: 5, title: "오설록", type: "Responsive Web", font: "Noto Sans Korean", src: process.env.PUBLIC_URL + "/assets/img/ndg.jpg", link: "http://desipossa.github.io/", anchor: "오설록" },
+];
+
+
+export { portfolioSub };
